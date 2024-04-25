53b411bf07194e5e82039d2205e11855159d3b1e
haikalbintang <mhaikalbintang.work@gmail.com>
2024-04-25 19:10:08 +0700

    Sign-up Modal is functional, but still buggy.

--- Frontend/final-project-revou/src/components/RegisterModal/index.tsx
+++ Frontend/final-project-revou/src/components/RegisterModal/index.tsx
@@ -1,31 +1,47 @@
-import {
-  ControllerFieldState,
-  ControllerRenderProps,
-  FieldValues,
-  UseFormStateReturn,
-} from "react-hook-form";
-import { ReactElement, JSXElementConstructor } from "react";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import Modal from "../Modal";
+import { Form } from "@/components/ui/form";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import Modal from "../Modal";
+import useMultistepForm from "@/hooks/useMultistepForm";
+import RegisterModalS1 from "../RegisterModalS1";
+import RegisterModalS2 from "../RegisterModalS2";
+import RegisterModalS3 from "../RegisterModalS3";
+
+const formSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(8),
+    passwordConfirm: z.string(),
+    username: z.string().min(2).max(50),
+    firstName: z.string(),
+    lastName: z.string(),
+    resetPasswordQuestion: z.string(),
+    resetPasswordAnswer: z.string(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirm;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["passwordConfirm"],
+    }
+  );
+
+// type FormFields = {
+//   email: string;
+//   password: string;
+//   passwordConfirm: string;
+//   username: string;
+//   firstName: string;
+//   lastName: string;
+//   resetPasswordQuestion: string;
+//   resetPasswordAnswer: string;
+// };
 
 type SetToggleMenuType = (
   value: boolean | ((prev: boolean) => boolean)
@@ -36,13 +52,28 @@ interface Props {
 }
 
 export default function RegisterModal({ setShowRegisterModal }: Props) {
-  // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
       username: "",
+      firstName: "",
+      lastName: "",
+      resetPasswordQuestion: "",
+      resetPasswordAnswer: "",
     },
   });
+
+  const { currentStepIndex, steps, isFirstStep, isLastStep, back, next, step } =
+    useMultistepForm([
+      <RegisterModalS1 key={1} form={form} />,
+      <RegisterModalS2 key={2} form={form} />,
+      <RegisterModalS3 key={3} form={form} />,
+    ]);
+
+  // 1. Define your form.
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -54,25 +85,30 @@ export default function RegisterModal({ setShowRegisterModal }: Props) {
   return (
     <Modal setShowModal={setShowRegisterModal}>
       <div className="p-6">
+        <h2>
+          {currentStepIndex + 1} / {steps.length}
+        </h2>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
+            {step}
+            <div className="flex justify-between">
+              {!isFirstStep ? (
+                <Button onClick={back}>Back</Button>
+              ) : (
+                <Button onClick={back} disabled>
+                  Back
+                </Button>
               )}
-            />
-            <Button type="submit">Submit</Button>
+              {isLastStep ? (
+                <Button className="bg-red-500 hover:bg-red-600" onClick={next}>
+                  Submit
+                </Button>
+              ) : (
+                <Button onClick={next} type={"submit"}>
+                  Next
+                </Button>
+              )}
+            </div>
           </form>
         </Form>
       </div>
