d9ff9bcbcf03ec145b7808eb4fde1fa7fbd61e6d
haikalbintang <mhaikalbintang.work@gmail.com>
2024-05-06 18:09:40 +0700

    Card_new is done, insyaAllah.

--- Frontend/final-project-revou/src/components/Card_new/index.tsx
+++ Frontend/final-project-revou/src/components/Card_new/index.tsx
@@ -33,7 +33,7 @@ export default function Card_new({
         <ImageCropper src={attachment} size={[300, 300]} />
         <div className="bg-slate-200 bg-opacity-65 absolute -bottom-2/3 hover:bottom-1 hover:duration-500 hover:ease-in-out left-1/2 -translate-x-1/2 w-11/12 h-40 p-2 rounded-xl border-4 border-solid border-white backdrop-blur-sm shadow-md shadow-slate-800">
           <div className="flex justify-between items-center">
-            <h1 className="text-sm font-semibold">{title}</h1>
+            <h1 className="text-sm font-semibold line-clamp-2">{title}</h1>
             <div className="p-1 bg-slate-800 text-slate-100 rounded-md">
               <h2 className="text-sm font-semibold">{nutriScore}</h2>
             </div>
@@ -61,9 +61,9 @@ export default function Card_new({
               {complexity}
             </button>
             <div className="mt-1">
-              <p className="text-xs">{description}</p>
+              <p className="text-xs line-clamp-2">{description}</p>
             </div>
-            <div className="flex justify-center items-center mt-2">
+            <div className="absolute bottom-0 right-0 -translate-x-9 flex justify-center items-center mt-0">
               <button className="text-sm rounded-xl px-2 py-1 bg-orange-600 hover:bg-orange-700 text-white">
                 More Info
               </button>
